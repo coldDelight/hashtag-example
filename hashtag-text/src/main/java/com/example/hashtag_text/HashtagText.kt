cb8851673a67e80eefb6883f9fbd3f60f7f60256
coldDelight <hno05039@naver.com>
2022-07-24 03:52:53 +0900

    글자수 제한 전까지

--- hashtag-text/src/main/java/com/example/hashtag_text/HashtagText.kt
+++ hashtag-text/src/main/java/com/example/hashtag_text/HashtagText.kt
@@ -1,52 +1,75 @@
 package com.example.hashtag_text
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.TextWatcher
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
 import android.util.AttributeSet
 import android.util.Log
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import java.util.regex.Pattern
 
 class HashtagText constructor(
     context: Context, attrs: AttributeSet?
 ): AppCompatEditText(context, attrs) {
-
-    private val tagList: ArrayList<String>? = null
     init {
-        attrs?.let {(it) }
+        attrs?.let { (it) }
     }
-
     override fun onTextChanged(
         text: CharSequence?,
         start: Int,
         lengthBefore: Int,
         lengthAfter: Int
     ) {
-        if (text != null) {
-            hashText(text)
+        val targetIndexList = indexOfAll(text.toString(), '#')
+        val hashIndexList = findHashIndex(text.toString(), targetIndexList)
+        if(targetIndexList.isNotEmpty()) {
+            for (i in 0 until targetIndexList.size) {
+                if (targetIndexList.size == hashIndexList.size) {
+                    this@HashtagText.text?.setSpan(
+                        ForegroundColorSpan(Color.GREEN),
+                        targetIndexList[i],
+                        hashIndexList[i],
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                }
+            }
         }
-
         super.onTextChanged(text, start, lengthBefore, lengthAfter)
     }
-    private fun hashText(text:CharSequence) {
 
-        val size: Int = text.length
-        val regex = Regex("^#\\s$")
+    private fun indexOfAll(text: String, target: Char): MutableList<Int> {
+        val targetIndexList = mutableListOf<Int>()
+        println(text)
+        for (i in text.indices) {
+            if (text[i] == target) {
+                targetIndexList.add(i)
+            }
+        }
+        return targetIndexList
+    }
 
-        val matchResult: Sequence<MatchResult> = regex.findAll("text")
-        Log.d("ww", "hashText: $matchResult")
-//        Log.d("ee", "hashText: ${ text.indexOf('#')}")
-
-
-//        if(size>0){
-//            for (i: Int in 0 until size) {
-////                Log.d("eee", "hashText: $i")
-//                if(text[i] == '#'){
-//                    val test :String = "sodded"
-//                    test.
-//                    Log.d("dddd", "hashText: ${text[i]}")
-//                }
-//            }
-//        }
-
+    private fun findHashIndex(text: String, targetIndexList: MutableList<Int>): MutableList<Int> {
+        val hashIndexList = mutableListOf<Int>()
+        for (i in targetIndexList) {
+            for (j in i + 1 until text.length) {
+                if (text[j] == ' ') {
+                    hashIndexList.add(j)
+                    break
+                } else if (text[j] == '#') {
+                    break
+                }
+            }
+        }
+        return hashIndexList
     }
 
 
